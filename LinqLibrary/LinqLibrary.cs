c89198b6ede02aba8a52289ca967d3d97bd040d4
Batista Pedro <Pedrocorreiabatista@gmail.com>
2022-04-09 19:48:00 +0200

    added new overload nad watch

--- LinqLibrary/LinqLibrary.cs
+++ LinqLibrary/LinqLibrary.cs
@@ -1,21 +1,47 @@
 ﻿using System;
 using System.Collections;
+using System.Diagnostics;
 using System.Collections.Generic;
 using System.Linq;
 namespace LinqLibrary;
 
-public static class OwnLinq
+public static class LinqSearch
 {
-    public static String[] ReturnIfContains<T>(T collection, char contains) where T : IEnumerable
+    public static List<String> ReturnIfContains<T>(T collection, char contains) where T : IEnumerable
     {
-        String[] result = new String[0];
+        var watch = new Stopwatch();
+        watch.Start();
+
+        List<String> result = new List<String>();
         foreach (var item in collection)
         {
             if (item.ToString().Contains(contains))
             {
-                result = result.Concat(new String[] { item.ToString() }).ToArray();
+                result = result.Concat(new List<String> { item.ToString() }).ToList();
             }
         }
+        watch.Stop();
+        Console.WriteLine($"Time elapsed for method: {watch.ElapsedMilliseconds} ms");
+
+        return result;
+    }
+
+    public static List<String> ReturnIfContains<T>(T collection, string contains) where T : IEnumerable
+    {
+        var watch = new Stopwatch();
+        watch.Start();
+
+        List<String> result = new List<String>();
+        foreach (var item in collection)
+        {
+            if (item.ToString().Contains(contains))
+            {
+                result = result.Concat(new List<String> { item.ToString() }).ToList();
+            }
+        }
+        watch.Stop();
+        Console.WriteLine($"Time elapsed for method: {watch.ElapsedMilliseconds} ms");
+
         return result;
     }
 }
